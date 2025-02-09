daaed607afc56e6cece650e43780668cb09a9ed5
sabinhamal <highsabin987@gmail.com>
2025-02-09 19:09:56 +0545

    first page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,33 @@
+import { Dot } from "lucide-react"
+import logo from "../assets/tree.png"
 
 const navbar = () => {
     return (
-        <div className="bg-transparent">navbar</div>
+        <div className="bg-transparent flex items-center justify-around w-full"
+            style={{ paddingTop: "24px", fontFamily: "Montserrat" }}>
+
+            <div className="flex items-end gap-4">
+                <img src={logo} className="w-9 h-9" alt="Tree Logo" />
+                <h1 className="font-bold text-xl text-[#033f2d]">FarmWise</h1>
+            </div>
+
+            <ul
+                className="flex items-center gap-4 text-[0.85rem] leading-5 font-medium w-[27%]">
+                <li>Home</li>
+                <li><Dot size={'17px'} /></li>
+                <li>About</li>
+                <li><Dot size={'17px'} /></li>
+                <li>Resources</li>
+                <li><Dot size={'17px'} /></li>
+                <li>Contact</li>
+            </ul>
+
+            {/* Menu */}
+            <div className="flex flex-col items-center gap-2">
+                <div className="h-[2px] bg-zinc-700 w-9"></div>
+                <div className="h-[2px] bg-zinc-700 w-9"></div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,46 @@
+import { Triangle } from "lucide-react"
 import Navbar from "../components/navbar"
+import Cards from "../components/index/cards"
 
 const index = () => {
     return (
-        <div className="bg-zinc-100">
+        <div
+            style={{ paddingBottom: "32px" }}
+            className="w-full h-screen flex flex-col items-center justify-between gap-12 text-center">
             <Navbar />
+
+            <div className="flex flex-col items-center gap-6">
+                <h1
+                    style={{ marginTop: "64px" }}
+                    className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
+                    Great harvests are built
+                    <br />
+                    with smart farming
+                </h1>
+
+                <p className="mx-auto mt-4 max-w-xl text-gray-500">
+                    The world's most advanced fertilizer recommendation platform, optimized for your farm in an easy way
+                </p>
+
+                <div className="flex items-center gap-4">
+                    <button
+                        style={{ padding: "15px 35px" }}
+                        className="bg-[#222222] hover:bg-zinc-900 cursor-pointer transition-all text-white rounded-full text-sm"
+                    >
+                        Get Started
+                    </button>
+                    <button
+                        style={{ padding: "15px 30px" }}
+                        className="bg-gray-200 hover:bg-gray-300 cursor-pointer transition-all rounded-full flex items-center gap-2 text-sm"
+                    >
+                        <Triangle size={'12px'} className="text-black fill-black rotate-90" />
+                        Watch Video
+                    </button>
+                </div>
+            </div>
+
+            {/* Cards Section */}
+            <Cards />
         </div >
     )
 }
